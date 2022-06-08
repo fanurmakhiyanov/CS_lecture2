6e0a6eec79283d7a465367c7be0bb496caddfc35
fanur <makhiyanov.ff@gmail.com>
2022-06-08 23:01:08 +0300

    Lecture2_Array

--- Example010_IntroArray/Program.cs
+++ Example010_IntroArray/Program.cs
@@ -5,17 +5,12 @@
     if (arg3 > result) result = arg3;
     return result;
 }
-int[] array
+//              0    1   2   3   4   5   6   7   8
+int[] array = { 21, 32, 43, 24, 35, 16, 17, 88, 89 };
 
-
-// int max1 = Max(a1, b1, c1);
-// int max2 = Max(a2, b2, c2);
-// int max3 = Max(a3, b3, c3);
-// int max = Max(max1, max2, max3);
-
-int max = Max(
-    Max(a1, b1, c1),
-    Max(a2, b2, c2),
-    Max(a3, b3, c3));
-
-Console.WriteLine(max);
+int result = Max(
+    Max(array[0], array[1], array[2]),
+    Max(array[3], array[4], array[5]),
+    Max(array[6], array[7], array[8])
+);
+Console.WriteLine(result);
